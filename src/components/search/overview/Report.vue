59e05efbe0121f5aa2f92fc0a15edf39db6c8a32
Topdevman <pkpavlo27@yahoo.com>
2017-06-27 12:21:48 +0300

    fix highchart error

--- src/components/search/overview/Report.vue
+++ src/components/search/overview/Report.vue
@@ -65,7 +65,7 @@
 
         </div>
         <div class="margin-top-20">
-          <highcharts v-bind:options="options" ref="highcharts"></highcharts>
+          <!-- <highcharts v-bind:options="options" ref="highcharts"></highcharts> -->
         </div>
 
       </div>
@@ -175,9 +175,6 @@
   import Vue from 'vue'
   import VueHighCharts from 'vue-highcharts'
   import * as Highcharts from 'highcharts'
-  import 'highcharts-more'
-
-  // import $ from 'jquery'
 
   window.Highcharts = Highcharts
   
@@ -218,56 +215,14 @@
             name: '',
             data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
           }]
-        },
-        heatoptions: {
-          chart: {
-            type: 'bar',
-            marginTop: 10,
-            marginBottom: 10
-          },
-          title: {
-            text: ''
-          },
-          xAxis: {
-            categories: ['12AM', '3AM', '6AM', '9AM', '12PM', '3PM', '6PM', '9PM', '12PM']
-          },
-          yAxis: {
-            categories: ['M', 'T', 'W', 'T', 'F'],
-            title: null
-          },
-          colorAxis: {
-            min: 0,
-            minColor: '#FFFFFF',
-            maxColor: Highcharts.getOptions().colors[0]
-          },
-          legend: {
-            align: 'right',
-            layout: 'vertical',
-            margin: 0,
-            verticalAlign: 'top',
-            y: 25,
-            symbolHeight: 320
-          },
-          series: [{
-            name: null,
-            borderWidth: 0,
-            data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84]],
-            dataLabels: {
-              enabled: true,
-              color: 'black',
-              style: {
-                textShadow: 'none'
-              }
-            }
-          }]
-        }
+        }        
       }
     },
     mounted: function () {
       if (!window.NAME_UNIQUE_FOR_THIS_FILE) {
         window.NAME_UNIQUE_FOR_THIS_FILE = true
         const mychart = document.getElementById('heatmap')
-        Highcharts.chart(mychart, this.heatoptions)
+        // Highcharts.chart(mychart, this.heatoptions)
       }
     }
   }
